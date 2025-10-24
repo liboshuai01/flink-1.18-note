179e03e7b1d05d83b363656577fa95914ee90fc3
Boshuai Li <liboshuai01@gmail.com>
2025-10-25 05:05:27 +0800

    add note

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/streamrecord/StreamRecord.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/streamrecord/StreamRecord.java
@@ -19,6 +19,7 @@ package org.apache.flink.streaming.runtime.streamrecord;
 
 import org.apache.flink.annotation.Internal;
 
+// TODO: 就是value值的一个包装类，多加了一个可选的时间戳字段
 /**
  * One value in a data stream. This stores the value and an optional associated timestamp.
  *
