7a9d2ff1a45e0cc8160d66675e359fdb083d8b36
Boshuai Li <liboshuai01@gmail.com>
2025-10-27 00:11:42 +0800

    add note

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/lbs/WordCount.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/lbs/WordCount.java
@@ -15,10 +15,11 @@ import org.apache.flink.util.Collector;
 
 public class WordCount {
     public static void main(String[] args) throws Exception {
-        // TODO: idea中启动被创建StreamExecutionEnvironment的子类LocalStreamEnvironment
-        // TODO: 然后设置一些必需配置项目，例如设置并行度为cpu核心数、部署目标为local、提交作业的方式为附加模式
-        // TODO: 此时env中只有这些简单的配置信息
+        // TODO: Idea中启动会创建一个LocalStreamEnvironment，然后对LocalStreamEnvironment进行一些项目运行所需配置的初始化工作。
+        // TODO: 此时env中只有这些简单的配置信息: 并行度为cpu核心数、部署目标为local、提交作业的方式为附加模式等
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // TODO: 通过env创建了DataStream, DatStream持有env、transformation, transformation持有OperatorFactory.
+        // TODO: OperatorFactory持有Operator, Operator仅持有SocketTextStreamFunction.
         DataStreamSource<String> dataStream = env.socketTextStream("localhost", 7777);
         SingleOutputStreamOperator<String> mapDataStream = dataStream.map(String::toLowerCase);
         SingleOutputStreamOperator<Tuple2<String, Integer>> flatMapDataStream = mapDataStream.flatMap(new Splitter());
